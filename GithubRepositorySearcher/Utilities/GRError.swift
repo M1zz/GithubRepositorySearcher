df5ff67d15f11dc7d3f40c7229c7679ebe605630
hyunho.lee <mizzking75@gmail.com>
2021-06-21 01:04:52 +0900

    Add NetworkManager, Error

---
+++ GithubRepositorySearcher/Utilities/GRError.swift
@@ -0,0 +1,17 @@
+//
+//  GRError.swift
+//  GithubRepositorySearcher
+//
+//  Created by 이현호 on 2021/06/20.
+//
+
+import Foundation
+
+import Foundation
+
+enum GRError: String, Error {
+    case invalidUsername = "이 레파지토리 이름은 잘못된 요청입니다. 다시 요청해주세요."
+    case unableToComplete = "요청을 완료할 수 없습니다. 네트워크 연결을 확인해주세요."
+    case invalidResponse = "유효하지 않은 서버로 부터의 응답입니다. 다시 요청해주세요."
+    case invalidData = "서버로 받은 데이터가 유효하지 않습니다. 다시 요청해주세요."
+}
