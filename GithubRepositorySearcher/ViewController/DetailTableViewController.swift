eaaad0f8d90ae534243eb0acba5afddd15c9d871
hyunho.lee <mizzking75@gmail.com>
2021-06-20 23:28:43 +0900

    add tableview
    테이블 뷰를 하나 추가했습니다.

--- GithubRepositorySearcher/ViewController/DetailTableViewController.swift
+++ GithubRepositorySearcher/ViewController/DetailTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class DetailTableViewController: UIViewController {
 
     var repositoryName: String = ""
+    private var searchResultCellIdentifier = "SearchResultCell"
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -22,8 +23,36 @@ class DetailTableViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .systemBackground
         tableView.backgroundColor = .red
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.contentInsetAdjustmentBehavior = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         navigationItem.title = repositoryName
     }
+}
+
+extension DetailTableViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: searchResultCellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = "title"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    
 }
