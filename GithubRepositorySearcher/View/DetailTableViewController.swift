80cfb69651e70e27607b13105a4a00f772fd267f
hyunho.lee <mizzking75@gmail.com>
2021-06-20 12:33:09 +0900

    Add MainSearchView, DetailTableView

---
+++ GithubRepositorySearcher/View/DetailTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailTableViewController.swift
+//  GithubRepositorySearcher
+//
+//  Created by 이현호 on 2021/06/19.
+//
+
+import UIKit
+
+class DetailTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
---
+++ GithubRepositorySearcher/View/MainSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainSearchViewController.swift
+//  GithubRepositorySearcher
+//
+//  Created by 이현호 on 2021/06/19.
+//
+
+import UIKit
+
+class MainSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
