1246b0f7fcb48b88d22d5ed4027b1e65e7645cd4
hyunho.lee <mizzking75@gmail.com>
2021-06-21 01:45:15 +0900

    Fix detailTableView

---
+++ GithubRepositorySearcher/Extensions/UITableView+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UITableView+Ext.swift
+//  GithubRepositorySearcher
+//
+//  Created by 이현호 on 2021/06/21.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async {
+            self.reloadData()
+        }
+    }
+    
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
--- GithubRepositorySearcher/ViewController/DetailTableViewController.swift
+++ GithubRepositorySearcher/ViewController/DetailTableViewController.swift
@@ -12,11 +12,13 @@ final class DetailTableViewController: UIViewController {
     private var repositoryName: String = ""
     private var repositories: [Repository] = []
     private var searchResultCellIdentifier = "SearchResultCell"
-    
+    private let tableViewCellHeight: CGFloat = 80
+    private static let storyboardName: String = "Main"
+     
     @IBOutlet weak var tableView: UITableView!
     
     static func instantiateFromStoryboard(repositoryName: String) -> DetailTableViewController {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: String(describing: self)) as! DetailTableViewController
         viewController.repositoryName = repositoryName
         return viewController
@@ -38,6 +40,7 @@ final class DetailTableViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.contentInsetAdjustmentBehavior = .never
+        tableView.removeExcessCells()
     }
     
     private func getRepositories(repositoryName: String) {
@@ -47,9 +50,7 @@ final class DetailTableViewController: UIViewController {
             switch result {
             case .success(let repositories):
                 self.repositories = repositories.items
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
+                self.tableView.reloadDataOnMainThread()
             case .failure(let error):
                 print(error)
             }
@@ -64,12 +65,7 @@ extension DetailTableViewController: UITableViewDelegate, UITableViewDataSource 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let repository = repositories[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: searchResultCellIdentifier, for: indexPath)
-        
-        cell.textLabel?.text = repository.name
-        cell.detailTextLabel?.text = repository.owner?.login ?? "no owner"
-        
-        return cell
+        return setDisplayCell(with: repository, indexPath: indexPath)
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -77,6 +73,17 @@ extension DetailTableViewController: UITableViewDelegate, UITableViewDataSource 
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
+        return tableViewCellHeight
+    }
+    
+    private func setDisplayCell(with repository: Repository, indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: searchResultCellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = repository.name
+        cell.textLabel?.textColor = .orange
+        cell.detailTextLabel?.text = repository.owner?.login ?? "no owner"
+        cell.detailTextLabel?.textColor = .systemGray2
+        
+        return cell
     }
 }
